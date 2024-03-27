ae33edd387d9a9413910a69948554861ec91b30f
Manon Le Bras <mle-bras@bess-f2r2s5.clusters.42paris.fr>
2024-03-27 16:23:40 +0100

    Start working on ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,16 @@
+#include "ScalarConverter.hpp"
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+int main(int argc, char **argv){
+
+	if (argc != 2)
+	{
+		cout << "A string representation of a C++ literal required" << endl;
+		return 0;
+	}
+	cout << argv[1] << endl;
+}
